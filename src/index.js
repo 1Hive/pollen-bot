11c0e96a44706fdc2bec6f94f176cb2cee0782ea
Cristopher Ortega <ortega.cpp@gmail.com>
2020-09-23 17:03:29 -0400

    Fixed graphql error & some linting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const Discord = require('discord.js')
 const dotenv = require('dotenv')
 const detectHandler = require('./parser/detectHandler')
-const {
-  RequestHandlerError,
-} = require('./error-utils')
+const { RequestHandlerError } = require('./error-utils')
 const { log } = require('./utils')
 
 // Load this as early as possible, to init all the environment variables that may be needed
@@ -16,42 +14,44 @@ client.on('ready', () => {
   log(`Bot successfully started as ${client.user.tag} 🐝`)
 })
 
-client.on('message', async message => {
-  if(message.author.bot) return
+client.on('message', async (message) => {
+  if (message.author.bot) return
 
   try {
     if (message.content.includes('app.brightid.org/connection-code')) {
       // Deletes the message inmediately.
-      message.delete({ timeout: 500 })
+      await message.delete({ timeout: 500 })
 
       // Sends a PM to the user, letting them know it is agains't the rules.
-      message.author.send({
-        'embeds': [
-          {
-            'title': 'Warning 🚨',
-            'description': 'You should not send BrightID connection links on public channels!',
-            'color': 16769024,
-            'fields': [
-              {
-                'name': 'Want to get verified?',
-                'value': 'The best way is to join a verification party at https://www.brightid.org/meet.'
-              }
-            ]
-          }
-        ],
+      await message.author.send({
+        embed: {
+          title: 'Warning 🚨',
+          description:
+            'You should not send BrightID connection links on public channels!',
+          color: 16769024,
+          fields: [
+            {
+              name: 'Want to get verified?',
+              value:
+                'The best way is to join a verification party at https://www.brightid.org/meet.',
+            },
+          ],
+        },
       })
-        
-      log(`Deleted message with BrightID connection link from ${message.author}.`)
+
+      log(
+        `Deleted message with BrightID connection link from ${message.author}.`,
+      )
     } else {
-      const handler = detectHandler(message.content)
-      handler(message)
+      const handler = await detectHandler(message.content)
+      await handler(message)
       log(
         `Served command ${message.content} successfully for ${message.author.username}.`,
       )
     }
   } catch (err) {
     if (err instanceof RequestHandlerError) {
-      message.reply(
+      await message.reply(
         'Could not find the requested command. Please use !ac help for more info.',
       )
     } else {
