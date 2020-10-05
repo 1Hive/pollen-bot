590c53741391cba2218d1464a4ccba7a4d6cc4f7
Jasper <51532010+yarode@users.noreply.github.com>
2020-10-05 07:29:04 +0200

    Command brightid (#4)
    * added brightid command
    
    * fixed channel ID

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -2,11 +2,13 @@
 const getHoneyAddy = require('./getHoneyAddy')
 const help = require('./help')
 const pollen = require('./pollen')
+const brightid = require('./brightid')
 
 const handlers = new Map([
   ['address', getHoneyAddy],
   ['help', help],
   ['pollen', pollen],
+  ['brightid', brightid],
 ])
 
 module.exports = handlers
