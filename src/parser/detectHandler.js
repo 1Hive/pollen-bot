ded529e88f4a237e7f3b61d4d0c8625e1fb8fe7e
Jasper <51532010+yarode@users.noreply.github.com>
2020-10-15 16:36:19 +0200

    Fix requested namespace (#13)
    * added getcred command
    
    * fixed typo in pollen.js
    
    * improved embed
    
    * command name change and help update
    
    * bot now only responds to namespace hny

--- src/parser/detectHandler.js
+++ src/parser/detectHandler.js
@@ -7,13 +7,13 @@ module.exports = function detectHandler(message) {
 
   const [requestedNamespace, requestedHandler] = message.split(' ')
   // If it's not a flag, we can safely ignore this command.
-  if (!requestedNamespace.includes('!')) {
+  if (!requestedNamespace.includes('!hny')) {
     return noop()
   }
-  
+
   const receivedHandler = handlers.get(requestedHandler)
 
-  if (typeof receivedHandler !== 'function' && requestedNamespace === '!hny') {
+  if (typeof receivedHandler !== 'function') {
     throw new RequestHandlerError(
       `Could not find command with flag ${requestedHandler}`,
     )
