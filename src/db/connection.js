a92bb00917dee5b43b4969132249555981c3dc69
yarode <jaspervdvorst@gmail.com>
2020-10-20 15:48:26 +0200

    update connection

---
+++ src/db/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const uri = process.env.MONGODB_URI
+mongoose.set('useUnifiedTopology', true)
+mongoose.set('useNewUrlParser', true)
+mongoose.connect(uri)
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('we are connected!')
+})
