78ebd80e58b54645d1ecb7b1b69e7d580a8a2efc
Vyom Jain <vyom.j@protonmail.com>
2021-01-20 19:03:18 +0530

    fix: updated links
    Removed old links and replaced them with the respective links to the wiki.
    Added a section for community covenant as well.

--- src/embed.js
+++ src/embed.js
@@ -13,7 +13,7 @@ function welcomeEmbed() {
         },
         {
           name: 'Frequently Asked Questions (FAQ) ❓',
-          value: 'https://hackmd.io/ObCMUJUxTumG2z6FJ5cnkg',
+          value: 'https://1hive.gitbook.io/1hive/guides/faq',
         },
         {
           name: 'Get free honey 🤑',
@@ -25,20 +25,23 @@ function welcomeEmbed() {
         },
         {
           name: 'Learn more about Honey (HNY) 🍯',
-          value: 'https://blog.1hive.org/honey/',
+          value: 'https://1hive.gitbook.io/1hive/projects/honey',
         },
         {
           name: 'Announcements and recaps of all the things 📰',
-          value: 'https://1hive.substack.com/ ',
+          value: 'https://1hive.gitbook.io/1hive/projects/milestones ',
         },
         {
           name: 'Documentation of our DAO 📝',
-          value: 'https://about.1hive.org/docs/dao/',
+          value: 'https://1hive.gitbook.io/1hive/',
         },
         {
-          name: 'Detailed description all of our channels',
-          value:
-            'https://discordapp.com/channels/698287700834517064/758821739202347038/758886904078008363',
+          name: 'Detailed description of all of our channels ℹ️',
+          value: 'https://1hive.gitbook.io/1hive/getting-started/discord#channels',
+        },
+        {
+          name: 'Our community covenant',
+          value: 'https://1hive.gitbook.io/1hive/community-covenant',
         },
       ],
       timestamp: new Date(),
