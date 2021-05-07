68574b0de6987d828e469a2dc9d3e62c7d16978b
Hernando Guzmán <hernando.guzman93@gmail.com>
2021-05-07 17:48:44 -0300

    Remove non pollen-related commands

--- src/handlers/address.ts
+++
@@ -1,7 +0,0 @@
-import { honeyAddy } from "../embed";
-import { Message } from "discord.js";
-
-export default async function address(message: Message): Promise<void> {
-  message.channel.send(`<@${message.author.id}>`);
-  message.channel.send(honeyAddy());
-}
