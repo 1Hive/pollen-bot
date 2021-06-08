43f270cb527aa3d828888c764053348af9622eeb
Hernando Guzmán <hernando.guzman93@gmail.com>
2021-06-08 15:34:37 -0300

    Onboarding handlers now check if a user is already created

--- src/handlers/github.ts
+++ src/handlers/github.ts
@@ -13,6 +13,11 @@ import { log } from "../utils";
 
 export async function verifyGithub(message: Message): Promise<void> {
   try {
+    const foundUser = await User.findOne({ discordId: message.author.id })
+
+    if (!foundUser) 
+      throw "You first need to save your wallet address with the `!pollen save-wallet <wallet-address>` command.";
+
     const [ username ] = parseGithubVerification(
       message.content,
       message.author.id,
@@ -23,15 +28,23 @@ export async function verifyGithub(message: Message): Promise<void> {
       message.author.send(response.message);
     }
   } catch (err) {
-    log(err);
-    message.reply(
-      "Command parsing failed. Please use the !pollen info command to see how to use the requested command properly."
-    );
+    if (typeof err === "string") message.reply(err)
+    else {
+      log(err);
+      message.reply(
+        "Command parsing failed. Please use the !pollen info command to see how to use the requested command properly."
+      );
+    }
   }
 };
 
 export async function checkGithub(message: Message): Promise<void> {
   try {
+    const foundUser = await User.findOne({ discordId: message.author.id })
+
+    if (!foundUser) 
+      throw "You first need to save your wallet address with the `!pollen save-wallet <wallet-address>` command.";
+
     const [ verification_code, username ] = parseGithubCheck(
       message.content,
       message.author.id,
@@ -47,19 +60,20 @@ export async function checkGithub(message: Message): Promise<void> {
       message.author.send(response.message);
 
       if (response.ok) {
-        await User.findOneAndUpdate(
-          { discordId: message.author.id, username: message.author.tag },
-          { github: username, modifiedAt: Date.now() },
-          { upsert: true, setDefaultsOnInsert: true }
+        await foundUser.updateOne(
+          { github: username, modifiedAt: Date.now() }
         );
   
         message.author.send("GitHub user succesfully saved.");
       }
     }
   } catch (err) {
-    log(err);
-    message.reply(
-      "Command parsing failed. Please use the !pollen info command to see how to use the requested command properly."
-    );
+    if (typeof err === "string") message.reply(err)
+    else {
+      log(err);
+      message.reply(
+        "Command parsing failed. Please use the !pollen info command to see how to use the requested command properly."
+      );
+    }
   }
 };
